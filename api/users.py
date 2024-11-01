a81f766f465cca659c1e39c56f5f9fe372915b30
ivertepny <ivertepny@gmail.com>
2024-11-01 19:28:10 +0200

    HW 9 completed

--- api/users.py
+++ api/users.py
@@ -1,9 +1,9 @@
 import json
 
-from fastapi import APIRouter, Response
+from fastapi import APIRouter, Response, HTTPException
 from pydantic import ValidationError
 
-from db import fetch_all_users, create_user_in_db, update_user_in_db, fetch_user
+from db import fetch_all_users, create_user_in_db, update_user_in_db, fetch_user, delete_user_from_db
 from models.user import UserData
 
 users_router = APIRouter(prefix="/users")
@@ -48,3 +48,33 @@ async def partial_update_user(user_id: int, user_data: dict):
         )
 
     return await update_user_in_db(user_id, user)
+
+
+@users_router.delete("/{user_id}", status_code=200)
+async def delete_user(user_id: int):
+    user = await fetch_user(user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    await delete_user_from_db(user_id)
+    return Response(
+        headers={"Content-Type": "application/json"},
+        content=json.dumps({"message": "User deleted successfully"}),
+        status_code=200
+    )
+
+#  Додав про всяк випадок варіант 2:
+#  якщо не відправляти повідомлення в тілі відповіді- то код 204
+#  а то знову мені за ці коди оцінку знизять )))
+
+
+# @users_router.delete("/{user_id}", status_code=204)
+# async def delete_user(user_id: int):
+#     user = await fetch_user(user_id)
+#
+#     if not user:
+#         raise HTTPException(status_code=404, detail="User not found")
+#
+#     await delete_user_from_db(user_id)
+#
+#     return Response(status_code=204)
